82c5df34565d94814c0181c0646b638d29f1d5d8
shivam.rai <shivam.rai@loblaw.ca>
2023-10-30 18:24:18 +0530

    "initial commit"

---
+++ src/test/java/com/learning/emsbackendservice/EmsBackendServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learning.emsbackendservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmsBackendServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
