ac25901068adba39ee1f5996778d01da90880919
shivam.rai <shivam.rai@loblaw.ca>
2023-10-31 03:48:43 +0530

    api for creating Employee added

---
+++ src/main/java/com/learning/emsbackendservice/service/EmployeeService.java
@@ -0,0 +1,7 @@
+package com.learning.emsbackendservice.service;
+
+import com.learning.emsbackendservice.dto.EmployeeDto;
+
+public interface EmployeeService{
+    EmployeeDto createEmployee(EmployeeDto employeeDto);
+}
